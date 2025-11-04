0501c845544342ae9b0d82ca3d0383565a422279
yh109 <yh599@qq.com>
2025-11-04 11:15:34 +0800

    Update main.R

--- 05_SSS_nihce/main.R
+++ 05_SSS_nihce/main.R
@@ -8,7 +8,8 @@
 # -----------------------------
 # 0. 环境设置
 # -----------------------------
-setwd("/data/home/quj_lab/zhangbin/result/neutrophil_aging/mouse_clock")
+# 设置当前工作目录为脚本所在路径
+setwd("/dellstorage09/quj_lab/yanghang/out_file/05_SSS_nihce")
 
 output_dir <- "/dellstorage09/quj_lab/yanghang/out_file"
 dir.create(output_dir, showWarnings = FALSE, recursive = TRUE)
