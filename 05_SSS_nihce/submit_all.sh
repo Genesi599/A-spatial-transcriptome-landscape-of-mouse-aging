c0623a12d476f8b94f59244f3893a602f015d6ab
yh109 <yh599@qq.com>
2025-11-12 14:53:13 +0800

    1

---
+++ 05_SSS_nihce/submit_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+GENE_LIST_DIR="/dellstorage09/quj_lab/yanghang/spatial/ref"
+
+N_GENE_LISTS=$(ls "$GENE_LIST_DIR"/*.txt 2>/dev/null | wc -l)
+
+if [[ $N_GENE_LISTS -eq 0 ]]; then
+    echo "Error: No .txt files found in $GENE_LIST_DIR"
+    exit 1
+fi
+
+echo "Found $N_GENE_LISTS gene list files"
+echo "Submitting array job: 1-${N_GENE_LISTS}"
+
+qsub -t 1-${N_GENE_LISTS} submit_niche_analysis.pbs
+
+echo "Jobs submitted successfully"
